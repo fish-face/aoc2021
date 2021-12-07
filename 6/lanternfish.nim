6dfbe840e06f50742b938eea4cb73bf09bcbccc4
Chris Le Sueur <c.m.lesueur@gmail.com>
2021-12-07 01:42:46 +0000

    Day 6

--- 6/lanternfish.nim
+++ 6/lanternfish.nim
@@ -10,24 +10,37 @@ proc `+=`(a: var openArray[int], b: openArray[int]) =
 let input = paramStr(1).readFile.strip().split(',').map(parseInt)
 
 var fish = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-var nextFish = fish
 for n in input:
   inc fish[n]
-#
-# for _ in 0..<11:
-#   for n in 0..<9:
-#     nextFish[(n+2) mod 9] = fish[n]
-#   fish += nextFish
-#
-#   # echo fish
 
-for _ in 0..<70:
-  for n in 1..<9:
-    nextFish[n-1] = fish[n]
-  nextFish[6] += fish[0]
-  nextFish[8] = fish[0]
-  fish = nextFish
+proc solve(fish: array[9, int], days: int): int =
+  var locFish = fish
+  var nextFish = fish
 
-echo fish
+  var weeks = days div 7
+  var remdays = days mod 7
 
-echo fish.foldl(a + b)
+  echo locFish
+  echo locFish.foldl(a+b)
+
+  for w in 0..<weeks:
+    for n in 0..<9:
+      nextFish[(n+2) mod 9] = locFish[n]
+    locFish[7] = 0
+    locFish[8] = 0
+    locFish += nextFish
+
+    echo locFish
+    echo locFish.foldl(a+b)
+
+  for _ in 0..<remdays:
+    for n in 1..<9:
+      nextFish[n-1] = locFish[n]
+    nextFish[6] += locFish[0]
+    nextFish[8] = locFish[0]
+    locFish = nextFish
+
+  locFish.foldl(a+b)
+
+echo solve(fish, 80)
+echo solve(fish, 256)
