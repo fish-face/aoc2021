fd71376cbfec318b33f9cd94ac7b4ad7ae89e42d
Chris Le Sueur <chris.lesueur@unipart.io>
2021-12-10 13:49:38 +0000

    Day 10

---
+++ 10/syntax_scoring.nim
@@ -0,0 +1,47 @@
+include prelude
+import algorithm
+
+let SCORES = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137
+}.toTable
+
+let COMPLETE_SCORES = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}.toTable
+
+let input = paramStr(1).readFile.strip.splitLines
+
+proc both() =
+  var partOne = 0
+  var scores: seq[int] = @[]
+  for line in input:
+    var stack: seq[char] = @[]
+    var incomplete = false
+    for c in line:
+      case c:
+        of '(': stack.add(')')
+        of '{': stack.add('}')
+        of '[': stack.add(']')
+        of '<': stack.add('>')
+        of ')', '}', ']', '>':
+          if c != stack.pop:
+            partOne += SCORES[c]
+            incomplete = true
+            break
+        else:
+          break
+    if incomplete:
+      var score = 0
+      for i, c in stack.reversed:
+        score = score * 5 + COMPLETE_SCORES[c]
+      scores.add(score)
+  echo partOne
+  echo scores.sorted[scores.len div 2]
+
+both()
