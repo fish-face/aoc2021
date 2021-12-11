e78b6b2996136437c5d0a50178a454d9e79ef89e
Chris Le Sueur <c.m.lesueur@gmail.com>
2021-12-11 17:27:04 +0000

    Fix Day 9

--- 9/main.nim
+++ 9/main.nim
@@ -17,37 +17,43 @@ const neighbours = [
   Coord(x:  0, y:  1),
 ]
 
-# preprocess input by adding a border of 9s for part 1 :)
-var input = paramStr(1).readFile.strip.splitLines.mapIt(it.toSeq.map(x => ($x).parseInt))
+var input = paramStr(1).readFile.strip.splitLines
+
 let
   w = input[0].len
   h = input.len
 var
   centers: seq[Coord] = @[]
+  border = newString(w)
+
+for i in 0..<w:
+  border[i] = '9'
+
+var preprocessed = (border & input & border).mapIt(('9' & it & '9').toSeq.map(x => ($x).parseInt))
 
 proc markComponent(grid: var seq[seq[int]], start: Coord, label: int): int =
   for n in neighbours:
     let sn = start + n
-    if sn.y > 0 and sn.y < h-1 and sn.x > 0 and sn.x < w-1 and grid[sn.y][sn.x] >= 0 and grid[sn.y][sn.x] < 9:
+    if sn.y > 0 and sn.y <= h and sn.x > 0 and sn.x <= w and grid[sn.y][sn.x] >= 0 and grid[sn.y][sn.x] < 9:
       grid[sn.y][sn.x] = label
       result += 1 + markComponent(grid, sn, label)
 
 proc partOne(): int =
-  for x in 1..<w-1:
-    for y in 1..<h-1:
+  for x in 1..w:
+    for y in 1..h:
       if (
-        input[y][x] < input[y-1][x] and
-        input[y][x] < input[y+1][x] and
-        input[y][x] < input[y][x-1] and
-        input[y][x] < input[y][x+1]
+        preprocessed[y][x] < preprocessed[y-1][x] and
+        preprocessed[y][x] < preprocessed[y+1][x] and
+        preprocessed[y][x] < preprocessed[y][x-1] and
+        preprocessed[y][x] < preprocessed[y][x+1]
       ):
-        result += input[y][x] + 1
+        result += preprocessed[y][x] + 1
         centers.add Coord(x: x, y: y)
 
 proc partTwo(): int =
   var topThree = @[0, 0, 0]
   for i, c in centers:
-    let size = markComponent(input, c, -1-i)
+    let size = markComponent(preprocessed, c, -1-i)
     if size > topThree[0]:
       topThree = @[size, topThree[0], topThree[1]]
     elif size > topThree[1]:
