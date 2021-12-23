99ce1eb362b3333ed22502468abdff7c5154c49f
Chris Le Sueur <c.m.lesueur@gmail.com>
2021-12-23 01:12:33 +0000

    Day 21 part 2

--- 21/main.nim
+++ 21/main.nim
@@ -3,35 +3,93 @@ include prelude
 import strscans
 import math
 
-var lastRoll = 99
-var rolls = 0
-
-proc deterministicDie(): int =
-  lastRoll = (lastRoll + 1) mod 100
-  rolls += 1
-  return lastRoll + 1
-
-proc rollThrice(): array[3, int] =
-  [deterministicDie(), deterministicDie(), deterministicDie()]
+type
+  State = object
+    p1, p2, s1, s2, next: int
+  Result = tuple
+    p1, p2: int
 
 let input = paramStr(1).readFile.strip.splitLines
 var (success1, player1) = input[0].scanTuple("Player 1 starting position: $i")
 var (success2, player2) = input[1].scanTuple("Player 2 starting position: $i")
-var score1, score2, loser = 0
 
-player1 = (player1 + 9) mod 10
-player2 = (player2 + 9) mod 10
+let state = State(p1: player1 - 1, p2: player2 - 1, next: 1)
 
-while true:
-  player1 = (player1 + sum rollThrice()) mod 10
-  score1 += player1 + 1
-  if score1 >= 1000:
-    loser = score2
-    break
-  player2 = (player2 + sum rollThrice()) mod 10
-  score2 += player2 + 1
-  if score2 >= 1000:
-    loser = score1
-    break
+proc partOne(start: State): int =
+  var lastRoll = 99
+  var rolls = 0
 
-echo loser * rolls
+  proc deterministicDie(): int =
+    lastRoll = (lastRoll + 1) mod 100
+    rolls += 1
+    return lastRoll + 1
+
+  proc rollThrice(): array[3, int] =
+    [deterministicDie(), deterministicDie(), deterministicDie()]
+
+  var state = start
+  var loser = 0
+
+  while true:
+    state.p1 = (state.p1 + sum rollThrice()) mod 10
+    state.s1 += state.p1 + 1
+    if state.s1 >= 1000:
+      loser = state.s2
+      break
+    state.p2 = (state.p2 + sum rollThrice()) mod 10
+    state.s2 += state.p2 + 1
+    if state.s2 >= 1000:
+      loser = state.s1
+      break
+
+  loser * rolls
+
+proc partTwo(start: State): int =
+  proc move1(s: State, m: int): State =
+    result = s
+    result.p1 = (result.p1 + m) mod 10
+    result.s1 += result.p1 + 1
+    result.next = 2
+  proc move2(s: State, m: int): State =
+    result = s
+    result.p2 = (result.p2 + m) mod 10
+    result.s2 += result.p2 + 1
+    result.next = 1
+
+  proc `*`(a: int, b: Result): Result =
+    (a * b.p1, a * b.p2)
+  proc `+`(a, b: Result): Result =
+    (a.p1 + b.p1, a.p2 + b.p2)
+
+  var memo: Table[State, Result]
+  proc step(state: State): Result =
+    if memo.hasKey(state): return memo[state]
+    if state.s1 >= 21: result = (1, 0)
+    elif state.s2 >= 21: result = (0, 1)
+    elif state.next == 1:
+      result = (result +
+        step(move1(state, 3)) +
+        3 * step(move1(state, 4)) +
+        6 * step(move1(state, 5)) +
+        7 * step(move1(state, 6)) +
+        6 * step(move1(state, 7)) +
+        3 * step(move1(state, 8)) +
+        step(move1(state, 9))
+      )
+    elif state.next == 2:
+      result = (result +
+        step(move2(state, 3)) +
+        3 * step(move2(state, 4)) +
+        6 * step(move2(state, 5)) +
+        7 * step(move2(state, 6)) +
+        6 * step(move2(state, 7)) +
+        3 * step(move2(state, 8)) +
+        step(move2(state, 9))
+      )
+    memo[state] = result
+
+  let (p1wins, p2wins) = step(start)
+  max(p1wins, p2wins)
+
+echo partOne(state)
+echo partTwo(state)
