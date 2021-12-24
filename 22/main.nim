0e6c3f25e42853ae255b0853323f749738a8813c
Chris Le Sueur <c.m.lesueur@gmail.com>
2021-12-24 14:37:01 +0000

    Day 22: Tidy and optimise

--- 22/main.nim
+++ 22/main.nim
@@ -6,57 +6,58 @@ import strscans
 type
   Rect = tuple
     x1, y1, z1, x2, y2, z2: int
-    on: bool
+    sgn: int
 
 proc fromString(line: string): Rect =
-  var on: bool
+  var sgn: int
   let (success, onoroff, x1, x2, y1, y2, z1, z2) = line.scanTuple("$w x=$i..$i,y=$i..$i,z=$i..$i")
   if onoroff == "on":
-    on = true
+    sgn = 1
   elif onoroff == "off":
-    on = false
+    sgn = -1
   else:
     raise new ValueError
-  (x1, y1, z1, x2+1, y2+1, z2+1, on)
-
-proc turnOn(r: Rect): Rect =
-  if r.on:
-    return r
-  else:
-    result = r
-    result.on = true
-
-proc empty(r: Rect): bool =
-  r.x1 >= r.x2 or r.y1 >= r.y2 or r.z1 >= r.z2
+  (x1, y1, z1, x2+1, y2+1, z2+1, sgn)
 
 proc `-`(r: Rect): Rect =
   result = r
-  result.on = not result.on
+  result.sgn = -result.sgn
 
 proc `*`(a, b: Rect): Rect =
   (
     max(a.x1, b.x1), max(a.y1, b.y1), max(a.z1, b.z1),
     min(a.x2, b.x2), min(a.y2, b.y2), min(a.z2, b.z2),
-    b.on
+    1
   )
 
 proc `*`(r: Rect, rr: seq[Rect]): seq[Rect] =
-  rr.mapIt(r * it).filterIt(not it.empty)
+  proc empty(r: Rect): bool =
+    r.x1 >= r.x2 or r.y1 >= r.y2 or r.z1 >= r.z2
 
-proc vol(r: Rect): int =
-  (r.x2 - r.x1) * (r.y2 - r.y1) * (r.z2 - r.z1) * [-1, 1][r.on.int]
+  rr.mapIt(r * it).filterIt(not it.empty)
 
 let input = paramStr(1).readFile.strip.splitLines.map(fromString)
 
-proc allIntersections(rects: seq[Rect], sgn: int): seq[Rect] =
+proc allIntersections(rects: seq[Rect], add: bool): seq[Rect] =
   for i, r in rects:
-    if sgn * r.on.int == 1:
-      result = result & @[r] & allIntersections(r * rects[i+1..^1].map(turnOn), -sgn)
-    elif sgn * r.on.int == -1:
-      result = result & @[-r] & allIntersections(r * rects[i+1..^1].map(turnOn), -sgn)
+    if r.sgn == -1:
+      continue
+
+    let
+      signed = if add: @[r] else: @[-r]
+      intersected = allIntersections(r * rects[i+1..^1], not add)
+
+    result = result & signed & intersected
 
 proc countOn(rects: seq[Rect]): int =
-  allIntersections(rects, 1).map(vol).sum
+  proc vol(r: Rect): int =
+    (r.x2 - r.x1) * (r.y2 - r.y1) * (r.z2 - r.z1) * r.sgn
 
-echo countOn(input.filterIt(it.x1 >= -50 and it.y1 >= -50 and it.z1 >= -50 and it.x2 <= 50 and it.y2 <= 50 and it.z2 <= 50))
-echo countOn(input)
+  allIntersections(rects, true).map(vol).sum
+
+proc isPartOne(r: Rect): bool =
+  r.x1 >= -50 and r.y1 >= -50 and r.z1 >= -50 and r.x2 <= 50 and r.y2 <= 50 and r.z2 <= 50
+
+let partOne = countOn(input.filter(isPartOne))
+echo partOne
+echo partOne + countOn(input.filterIt(not it.isPartOne))
