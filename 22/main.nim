476d3bd8413ebacf8292b0056538fa4903b4bd2f
Chris Le Sueur <c.m.lesueur@gmail.com>
2021-12-24 12:19:27 +0000

    Day 22 intersection counting

--- 22/main.nim
+++ 22/main.nim
@@ -43,74 +43,20 @@ proc `*`(a, b: Rect): Rect =
 proc `*`(r: Rect, rr: seq[Rect]): seq[Rect] =
   rr.mapIt(r * it).filterIt(not it.empty)
 
-proc `-`(a, b: Rect): seq[Rect] =
-  @[
-    a * (a.x1, a.y1, a.z1, b.x1, b.y1, b.z1, a.on),
-    a * (b.x1, a.y1, a.z1, b.x2, b.y1, b.z1, a.on),
-    a * (b.x2, a.y1, a.z1, a.x2, b.y1, b.z1, a.on),
-
-    a * (a.x1, b.y1, a.z1, b.x1, b.y2, b.z1, a.on),
-    a * (b.x1, b.y1, a.z1, b.x2, b.y2, b.z1, a.on),
-    a * (b.x2, b.y1, a.z1, a.x2, b.y2, b.z1, a.on),
-
-    a * (a.x1, b.y2, a.z1, b.x1, a.y2, b.z1, a.on),
-    a * (b.x1, b.y2, a.z1, b.x2, a.y2, b.z1, a.on),
-    a * (b.x2, b.y2, a.z1, a.x2, a.y2, b.z1, a.on),
-
-
-    a * (a.x1, a.y1, b.z1, b.x1, b.y1, b.z2, a.on),
-    a * (b.x1, a.y1, b.z1, b.x2, b.y1, b.z2, a.on),
-    a * (b.x2, a.y1, b.z1, a.x2, b.y1, b.z2, a.on),
-
-    a * (a.x1, b.y1, b.z1, b.x1, b.y2, b.z2, a.on),
-    # a * (b.x1, b.y1, b.z1, b.x2, b.y2, b.z2, b.on),
-    a * (b.x2, b.y1, b.z1, a.x2, b.y2, b.z2, a.on),
-
-    a * (a.x1, b.y2, b.z1, b.x1, a.y2, b.z2, a.on),
-    a * (b.x1, b.y2, b.z1, b.x2, a.y2, b.z2, a.on),
-    a * (b.x2, b.y2, b.z1, a.x2, a.y2, b.z2, a.on),
-
-
-    a * (a.x1, a.y1, b.z2, b.x1, b.y1, a.z2, a.on),
-    a * (b.x1, a.y1, b.z2, b.x2, b.y1, a.z2, a.on),
-    a * (b.x2, a.y1, b.z2, a.x2, b.y1, a.z2, a.on),
-
-    a * (a.x1, b.y1, b.z2, b.x1, b.y2, a.z2, a.on),
-    a * (b.x1, b.y1, b.z2, b.x2, b.y2, a.z2, a.on),
-    a * (b.x2, b.y1, b.z2, a.x2, b.y2, a.z2, a.on),
-
-    a * (a.x1, b.y2, b.z2, b.x1, a.y2, a.z2, a.on),
-    a * (b.x1, b.y2, b.z2, b.x2, a.y2, a.z2, a.on),
-    a * (b.x2, b.y2, b.z2, a.x2, a.y2, a.z2, a.on),
-  ].filterIt(not it.empty)
-
-proc `-*-`(a, b: Rect): seq[Rect] =
-  if a.on and b.on:
-    if a * b == b:
-      return @[]
-    else:
-      return a - b
-  elif a.on and not b.on:
-    return a - b
-  elif not a.on and b.on:
-    return a - b
-  elif not a.on and not b.on:
-    return @[a, -(a * b)]
-
 proc vol(r: Rect): int =
   (r.x2 - r.x1) * (r.y2 - r.y1) * (r.z2 - r.z1) * [-1, 1][r.on.int]
 
-proc flatten[T](s: seq[seq[T]]): seq[T] =
-  for x in s:
-    result &= x
-
 let input = paramStr(1).readFile.strip.splitLines.map(fromString)
 
+proc allIntersections(rects: seq[Rect], sgn: int): seq[Rect] =
+  for i, r in rects:
+    if sgn * r.on.int == 1:
+      result = result & @[r] & allIntersections(r * rects[i+1..^1].map(turnOn), -sgn)
+    elif sgn * r.on.int == -1:
+      result = result & @[-r] & allIntersections(r * rects[i+1..^1].map(turnOn), -sgn)
+
 proc countOn(rects: seq[Rect]): int =
-  var volume = @[rects[0]]
-  for i, r in rects[1..^1]:
-    volume = volume.mapIt(it -*- r).flatten & (if r.on: @[r] else: @[])
-  volume.map(vol).sum
+  allIntersections(rects, 1).map(vol).sum
 
 echo countOn(input.filterIt(it.x1 >= -50 and it.y1 >= -50 and it.z1 >= -50 and it.x2 <= 50 and it.y2 <= 50 and it.z2 <= 50))
-# echo countOn(input)
+echo countOn(input)
